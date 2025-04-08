a38153921c30ecf418b3c9043f92f6b539744f13
Jonoline <166339144+Jonoline@users.noreply.github.com>
2025-04-07 20:30:56 -0400

    Update HolaMundo.java
    hola

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,12 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class HolaMundo {
+    public class HolaMundo {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hola Mundo!!!");
-
-
-
+            System.out.println("Hello, World!");
+            System.out.println("Jonathan Chavez");
+        }
     }
-}
