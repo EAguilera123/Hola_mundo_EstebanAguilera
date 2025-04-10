ab3aec28db83c7a5f6ba8981688922a103c6cbed
joaquin <jm.arriagadapena2@gmail.com>
2025-04-09 22:06:34 -0400

    Hola mundo, soy Joaquin Arriagada

--- HolaMundo.java/src/Main.java
+++ HolaMundo.java/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World and make friends!");
+        System.out.println("Mi nombre es Joaquín Arriagada, un gusto :)");
     }
 }
