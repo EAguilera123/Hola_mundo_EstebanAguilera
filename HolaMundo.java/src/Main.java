76eb34eec1675b905c9a4a8d0be0f1a07ce94de1
joaquin <jm.arriagadapena2@gmail.com>
2025-04-12 19:01:45 -0400

    refactor: rename Main.java to Menu_Relu.java

--- HolaMundo.java/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World and make friends!");
-        System.out.println("Mi nombre es Joaquín Arriagada, un gusto :)");
-    }
-}
